203f9cda7f1481f9f4fc3b25cb08911f2f612210
Hemanth Kumar <digitalhemanth@gmail.com>
2022-06-01 23:55:08 +0530

    Update Mongo_connection.py

--- Mongo_connection.py
+++ Mongo_connection.py
@@ -2,10 +2,11 @@ from pymongo import MongoClient
 import pandas as pd
 
 client = MongoClient("localhost", 27017, maxPoolSize=50)
+'''
 db = client.mydb
 for db in client.list_databases():
     print(db)
-
-collection = db.Test
-data = list(collection.find())
+'''
+collection = db.mydb
+data = pd.DataFrame(list(collection.find()))
 print(data)
