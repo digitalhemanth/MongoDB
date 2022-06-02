52d2d54f5df25bf045a57e80e9f6f862fe2d1373
Hemanth Kumar <digitalhemanth@gmail.com>
2022-06-02 23:11:47 +0530

    Mongodb

--- Mongo_connection.py
+++ Mongo_connection.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
 import pandas as pd
 
-client = MongoClient("localhost", 27017, maxPoolSize=50)
-print("MongoDb DataBases ")
+client = MongoClient("localhost", 27017)
+print("MongoDb Databases ")
 data = pd.DataFrame(list(client.list_databases()))
 print(data)
-print("\n Collections list ")
+
+print("\n Collections List ")
 mydatabase = client.Test
 data = pd.DataFrame(list(mydatabase.list_collection_names()))
 print(data)
---
+++ mongo_operations.py
@@ -0,0 +1,36 @@
+from pydoc import cli
+from pymongo import MongoClient
+import pandas as pd
+
+client = MongoClient("localhost", 27017)
+print("\n Connected to ", client)
+def ShowDB():
+    print("\n MongoDb Databases ")
+    data = pd.DataFrame(list(client.list_databases()))
+    print(data)
+''''
+print("\n Collections List ")
+mydatabase = client.Test
+data = pd.DataFrame(list(mydatabase.list_collection_names()))
+print(data)
+'''
+def main():
+    choice = """ 
+      1 : Show Databases
+      2 : Create Database 
+      3 : Show Collections
+      4 : Create Collections 
+      5 : Find Doucuments 
+     """
+    print(choice)
+    Option = input("Please Choose a option:")
+    Option = int(Option)
+    try:
+        if Option == 1:
+            ShowDB()
+    except:
+        print("Sorry something went wrong !")
+    finally:
+        main()
+main()
+
