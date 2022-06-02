f26f9d4de858044d93682d4b4cb75ca2d4e5fb04
Hemanth Kumar <digitalhemanth@gmail.com>
2022-06-02 22:38:08 +0530

    Update Mongo_connection.py

--- Mongo_connection.py
+++ Mongo_connection.py
@@ -5,7 +5,7 @@ client = MongoClient("localhost", 27017, maxPoolSize=50)
 print("MongoDb DataBases ")
 data = pd.DataFrame(list(client.list_databases()))
 print(data)
-print("Collections list ")
+print("\n Collections list ")
 mydatabase = client.Test
 data = pd.DataFrame(list(mydatabase.list_collection_names()))
 print(data)
