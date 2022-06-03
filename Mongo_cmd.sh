20563a7ce9e4751690e1cd8f76faa6626ff0805b
Hemanth Kumar <digitalhemanth@gmail.com>
2022-06-03 22:58:13 +0530

    Update Mongo_cmd.sh

--- Mongo_cmd.sh
+++ Mongo_cmd.sh
@@ -93,3 +93,35 @@ PyDb> db.Products.find().pretty()
   }
 ]
 PyDb>
+
+Please enter a MongoDB connection string (Default: mongodb://localhost/):
+
+Current Mongosh Log ID: 629a4296aec53917e4102067
+Connecting to:          mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.4.2
+Using MongoDB:          5.0.8
+Using Mongosh:          1.4.2
+
+For mongosh info see: https://docs.mongodb.com/mongodb-shell/
+
+------
+   The server generated these startup warnings when booting:
+   2022-06-03T21:25:53.282+05:30: Access control is not enabled for the database. Read and write access to data and configuration is unrestricted
+------
+
+Warning: Found ~/.mongorc.js, but not ~/.mongoshrc.js. ~/.mongorc.js will not be loaded.
+  You may want to copy or rename ~/.mongorc.js to ~/.mongoshrc.js.
+test> show dbs
+PyDb    72.00 KiB
+Test    72.00 KiB
+admin   40.00 KiB
+config  72.00 KiB
+local   80.00 KiB
+test> use PyDb
+switched to db PyDb
+PyDb> show collections
+Products
+PyDb> db.Products.drop()
+true
+PyDb> show collections
+
+PyDb>
