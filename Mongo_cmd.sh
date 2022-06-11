51b23a052004cd46d977570f0a0a9765ebe2261f
Hemanth Kumar <digitalhemanth@gmail.com>
2022-06-11 22:03:24 +0530

    Update Mongo_cmd.sh

--- Mongo_cmd.sh
+++ Mongo_cmd.sh
@@ -2825,9 +2825,6 @@ PyDb> db.Product.find({item:"Mobile"})..explain()
 Uncaught:
 SyntaxError: Unexpected token (1:33)
 
-> 1 | db.Product.find({item:"Mobile"})..explain()
-    |                                  ^
-  2 |
 
 PyDb> db.Product.find({item:"Mobile"}).explain()
 {
